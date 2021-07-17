36e8ccfe82e0ff3ada308d18f3f58b789dc3048b
seliana <cany96@bk.ru>
2021-07-17 12:45:32 +0300

    style: colors and add trip button

--- src/client/constants/_url.constants.js
+++ src/client/constants/_url.constants.js
@@ -6,5 +6,5 @@ const BASE_URLS = {
 };
 
 // pixabay:  https://pixabay.com/api/?key=API_KEY&q=moscow&image_type=photo
-// weatherbit:  https://api.weatherbit.io/v2.0/history/daily?postal_code=27601&country=US&start_date=2021-07-12&end_date=2021-07-13&key=API_KEY
+// weatherbit:  https://api.weatherbit.io/v2.0/history/daily?&lat=38.123&lon=-78.543&start_date=2021-07-12&end_date=2021-07-13&key=API_KEY
 // geonames: http://api.geonames.org/citiesJSON?north=44.1&south=-9.9&east=-22.4&west=55.2&lang=de&username=API_KEY
--- src/client/js/helpers/_form_handler.js
+++ src/client/js/helpers/_form_handler.js
@@ -7,11 +7,6 @@ export const handleSubmit = (event) => {
   const articleUrl = document.getElementById('url').value;
   document.querySelector('#url').classList.remove('url_error');
 
-  // if (!TravelClient.urlIsValid(articleUrl)) {
-  //   document.querySelector('#url').classList.add('url_error');
-  //   return;
-  // }
-
   TravelClient.showPreloader();
   TravelClient.getArticleAnalysis(articleUrl);
 };
